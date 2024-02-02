ceff58f4df221163d0bd55b5ab5bde026bc340bc
ortega <kabweortega@gmail.com>
2024-02-02 16:25:34 +0200

    Feat: Add gemena server controller

---
+++ src/main/java/com/ortega/sgemena/SGemenaApplication.java
@@ -0,0 +1,13 @@
+package com.ortega.sgemena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SGemenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SGemenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ortega/sgemena/SGemenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.sgemena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SGemenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
