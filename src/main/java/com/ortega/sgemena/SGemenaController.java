876a10217f505eac46fdd1ee6e1dcf9e2ce38346
ortega <kabweortega@gmail.com>
2024-02-09 22:49:38 +0200

    Feat: Setup request exchange

--- src/main/java/com/ortega/sgemena/SGemenaController.java
+++ src/main/java/com/ortega/sgemena/SGemenaController.java
@@ -1,16 +1,21 @@
 package com.ortega.sgemena;
 
+import com.ortega.sgemena.services.InfoService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("api/v1")
 public class SGemenaController {
 
+    private InfoService infoService;
+
     @GetMapping("/info")
     public String index() {
-        return "Serveur de Gemena";
+        return infoService.getInfoServer();
     }
 
 }
---
+++ src/main/java/com/ortega/sgemena/services/InfoService.java
@@ -0,0 +1,9 @@
+package com.ortega.sgemena.services;
+
+import org.springframework.stereotype.Service;
+
+public interface InfoService {
+
+    String getInfoServer();
+
+}
